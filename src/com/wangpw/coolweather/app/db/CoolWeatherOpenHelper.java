f6a923be82504d7dec2f1c7953f8ecca71cd7841
pei6964012 <pei6964012@163.com>
2015-08-24 10:58:49 +0800

    DB help classes and the classes of the entities(City,Province,County)

---
+++ src/com/wangpw/coolweather/app/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,53 @@
+package com.wangpw.coolweather.app.db;
+
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper {
+	
+	/*
+	 * 	Province建表语句
+	 */
+	public static final String CREATE_PROVINCE = "create table Province ("
+			+"id integer primary key autoincrement, "
+			+"province_name text, "
+			+"province_code text)";
+	/*
+	 * 	City建表语句
+	 */
+	public static final String CREATE_CITY = "create table City ("
+			+"id integer primary key autoincrement, "
+			+"city_name text, "
+			+"city_code text, "
+			+"province_code text)";
+	/*
+	 * 	County建表语句
+	 */
+	public static final String CREATE_COUNTY = "create table County ("
+			+"id integer primary key autoincrement, "
+			+"county_name text, "
+			+"county_code text, "
+			+"city_id integer)";
+	
+	public CoolWeatherOpenHelper(Context context, String name,
+			CursorFactory factory, int version) {
+		super(context, name, factory, version);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		//创建省市县三张表
+		db.execSQL(CREATE_PROVINCE);
+		db.execSQL(CREATE_CITY);
+		db.execSQL(CREATE_COUNTY);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {		
+	}
+
+}
